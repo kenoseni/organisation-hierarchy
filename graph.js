9aff811c8eca9481ed4442569346271f363a6468
Olusola Oseni <kenolusola@gmail.com>
2020-02-29 12:15:51 +0100

    Set up project and connect database - set up body layout - add modal form to body - add button to trigger form - set up firebase connection - set up listening event on from database - add styles to form and body - create svg element and group to contain tree - add .gitignore file

---
+++ graph.js
@@ -0,0 +1,37 @@
+const dimensions = {
+    height: 500,
+    width: 1100
+}
+
+const svg = d3.select('.canvas')
+    .append('svg')
+    .attr('width', dimensions.width + 100)
+    .attr('height', dimensions.height + 100)
+
+const graph = svg.append('g')
+    .attr('transform', 'translate(50, 50)')
+
+let data = []
+
+db.collection('employees').onSnapshot(res => {
+    res.docChanges().forEach(change => {
+        const doc = {...change.doc.data(), id: change.doc.id }
+
+        switch (change.type) {
+            case 'added':
+                data.push(doc)
+                break;
+            case 'modified':
+                const index = data.findIndex(item => item.id == doc.id)
+                data[index] = doc
+                break;
+            case 'removed':
+                data = data.filter(item => item.id !== doc.id) 
+                break;
+            default:
+                break;  
+        }
+    })
+})
+
+
